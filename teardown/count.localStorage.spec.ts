ae94a8f3fc7f8c991ff02b198c3bee3248958189
qiuwenhui <1059713136@qq.com>
2025-02-05 21:42:26 +0800

    ✨ feat: 测试的拆卸

---
+++ teardown/count.localStorage.spec.ts
@@ -0,0 +1,20 @@
+import { test, afterEach, expect, describe, beforeEach } from "vitest";
+import { clearCount, getCount, increaseCount } from "./count.localStorage";
+describe("count", () => {
+  // teardown
+  afterEach(() => {
+    clearCount();
+  });
+
+  test("first should count ++", () => {
+    increaseCount("error");
+
+    expect(getCount()).toBe(1);
+  });
+
+  test("second should count ++", () => {
+    increaseCount();
+
+    expect(getCount()).toBe(1);
+  });
+});
